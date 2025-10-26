4371664ac59f04270b5da599f0c1338c384b0239
UsuarioGithub <correo@github.com>
2025-10-26 13:15:19 -0500

    tuto 2# Please enter the commit message for your changes. Lines starting

--- src/app/serie/serie.service.spec.ts
+++ src/app/serie/serie.service.spec.ts
@@ -1,16 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 
 import { SerieService } from './serie.service';
+
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 describe('SerieService', () => {
   let service: SerieService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SerieService);
-  });
+   TestBed.configureTestingModule({
+     imports: [HttpClientTestingModule],
+     providers: [SerieService]
+   });
+ });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+ it('should ...', inject([SerieService], (service: SerieService) => {
+   expect(service).toBeTruthy();
+ }));
 });
